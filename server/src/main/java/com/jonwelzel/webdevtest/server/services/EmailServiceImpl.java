2164c3234184f9d1c81990abff7b498fb993bc5e
Jonathan Welzel <jnwelzel@gmail.com>
2015-07-22 19:28:05 -0300

    email wip

--- server/src/main/java/com/jonwelzel/webdevtest/server/services/EmailServiceImpl.java
+++ server/src/main/java/com/jonwelzel/webdevtest/server/services/EmailServiceImpl.java
@@ -1,22 +1,26 @@
 package com.jonwelzel.webdevtest.server.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by jwelzel on 22/07/15.
  */
 public class EmailServiceImpl implements EmailServiceInterface {
+
+    private static final Logger log = LoggerFactory.getLogger("EmailServiceImpl");
 
     private Runnable sendEmailTask = new Runnable() {
         @Override
         public void run() {
             try {
                 Thread.sleep(3000L);
-                System.out.println("Thread executou");
+                log.info("E-mail enviado com sucesso");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     };
-
 
     @Override
     public void sendEmail(String from, String to, String title, String message) {
