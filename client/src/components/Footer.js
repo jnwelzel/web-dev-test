fd7a938df4d95755b759fd2090bbdb5a4eaeb4e7
Jonathan Welzel <jonathan.welzel@fluig.com>
2015-08-25 15:50:06 -0300

    client - add fork button

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,8 +13,8 @@ var Footer = React.createClass({
         <div className="grid grid-pad">
           <div className="col-1-1">
             <div className="content">
-              <span className="by">Created by <a href="http://jonwelzel.com" target="_blank">Jon Welzel</a></span>
-              <span className="for">For <a href="https://meuspedidos.com.br" target="_blank">Meus Pedidos</a> | <a className="source" href="https://github.com/jnwelzel/web-dev-test" target="_blank">Source code</a></span>
+              <span className="by"><iframe id="fork-me" src="https://ghbtns.com/github-btn.html?user=jnwelzel&repo=web-dev-test&type=fork&count=true" frameborder="0" scrolling="0" width="110px" height="20px"></iframe></span>
+              <span className="for">Created by <a href="http://jonwelzel.com" target="_blank">Jon Welzel</a> for <a href="https://meuspedidos.com.br" target="_blank">Meus Pedidos</a></span>
             </div>
           </div>
         </div>
